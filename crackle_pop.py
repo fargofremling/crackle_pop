cc5883c6d5f0e6a090640d98bcc20fb320aa7643
fargofremling <alicia.fremling@gmail.com>
2016-05-29 05:56:06 -0700

    new commit

---
+++ crackle_pop.py
@@ -0,0 +1,35 @@
+# GOAL
+# Write a program that prints out the numbers 1 to 100 (inclusive)
+
+# RULES
+# If the number is divisible by 3, print Crackle instead of the number.  
+# If it's divisible by 5, print Pop.
+# If it's divisible by both 3 and 5, print CracklePop.
+
+def crackle_pop():
+
+    number = 1
+
+    while number != 101:
+        if number % 3 and number % 5 == 0:
+        	print "CranklePop"
+        	number += 1
+        
+        if number % 3 == 0:
+        	print "Crackle"
+        	number += 1
+        
+        if number % 5 == 0:
+        	print "Pop"
+        	number += 1
+        
+        else:
+        	print number
+        	number += 1
+        
+crackle_pop()
+
+# Need to fix logic
+# 5 returns CracklPop
+# 15 returns Pop
+# Results should be reversed
